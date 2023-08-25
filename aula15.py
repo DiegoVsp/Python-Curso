3b4cc4eb66052a3d3ca2b25399ab72376e2cdbb2
DiegoVsp <diego-vespa2020@outlook.com.br>
2023-08-25 11:31:21 -0300

    formatação e condicionais em Python

---
+++ aula15.py
@@ -0,0 +1,7 @@
+# nome = input('Qual o seu nome? ')
+# print(f'Olá {nome}')
+numero_1 = input('Digite um número ')
+numero_2 = input('Digite um segundo número ')
+
+soma = int(numero_1) + int(numero_2)
+print(f'soma = {soma}')
